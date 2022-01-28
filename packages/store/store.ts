a4276f81f60d7d89f56b5dcae5413ee6a98733e8
Alex Ponce <avponce@hotmail.com>
2022-01-28 11:48:57 -0800

    add Web3 to project

--- packages/store/store.ts
+++ packages/store/store.ts
@@ -1,11 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import web3Reducer from './features/web3'
+import { initializeWeb3 } from './sagas/web3Saga'
+import createSagaMiddleware from 'redux-saga'
+
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
     reducer: { 
         web3: web3Reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
 })
+
+sagaMiddleware.run(initializeWeb3)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
