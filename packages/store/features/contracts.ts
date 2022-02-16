a4411c93bf00af5d99c09f7a0ffdbdf49ce43bcc
Alex Ponce <avponce@hotmail.com>
2022-02-16 12:56:28 -0800

    add contracts reducer

--- packages/store/features/contracts.ts
+++ packages/store/features/contracts.ts
@@ -2,26 +2,53 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { generateContractInitialState } from '../../utils/contractState'
 
 interface contracts {
-    initialized: boolean,
-    synced: boolean,
     [k: string]: any
 }
 
 interface contractsState {
-    [k: string]: contracts
+    [contractName: string]: contracts
 }
 
-const initialState = {}
+const initialState: contractsState = { }
 
 const contractsSlice = createSlice({
     name: 'contracts',
     initialState,
     reducers: {
-        initialized(state, action: any) {
-            [action.payload.contractConfig.contractName] = generateContractInitialState(action.contractConfig)
+        initializing(state, action: PayloadAction<{contractConfig: {[contractName: string]: any}}>) {
+            [action.payload.contractConfig.contractName] = generateContractInitialState(action.payload.contractConfig)
+        },
+        initialized(state, action: PayloadAction<{name: string}>) {
+            state[action.payload.name] = { 
+                initialized: true, 
+                synced: true, 
+                events: [] }
+        },
+        deleted(state, action: PayloadAction<{name:string}>) {
+            const { [action.payload.name]: omitted } = state
+        },
+        syncing(state, action: PayloadAction<{name: string}>) {
+            state[action.payload.name] = { synced: true }
+        },
+        gotvars(state, action: any) {
+            state[action.name][action.variable][action.argsHash] = { 
+                args: action.args, 
+                fnIndex: action.fnIndex, 
+                value: action.value, 
+                error: null }
+        },
+        gotvarserror(state, action: any) {
+            state[action.name][action.variable][action.argsHash] = { 
+                args: action.args, 
+                fnIndex: action.fnIndex, 
+                value: null, 
+                error: action.error }
+        },
+        eventfired(state, action: PayloadAction<{name: string, event: Array<any>}>) {
+            state[action.payload.name].events.push(action.payload.event)
         }
     }
 })
 
-export const { initialized } = contractsSlice.actions
+export const { initializing } = contractsSlice.actions
 export default contractsSlice.reducer
